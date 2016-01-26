8164b0717bde3f08a47a37ec5a44f82ed9201f93
Matteo Abis <gmatteo..abis@gmail.com>
2016-01-26 14:38:37 +0100

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/home/dota2responsesbot/dota2responsesbot/current"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/home/dota2responsesbot/dota2responsesbot/current/tmp/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/home/dota2responsesbot/dota2responsesbot/current/log/unicorn.log"
+stdout_path "/home/dota2responsesbot/dota2responsesbot/current/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.dota2responsesbot.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 3
+
+# Time-out
+timeout 30
