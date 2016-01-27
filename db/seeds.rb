73961e951964b50e7b1544850c467c860e326cb3
Matteo Abis <gmatteo..abis@gmail.com>
2016-01-27 16:43:46 +0100

    new database with 10k+ more responses

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 #
-heroes_filename = "db/heroes.json"
-responses_filename = "db/dota_responses_1.3.json"
+responses_filename = "db/dota_responses_1.4.json"
 
-heroes = JSON.parse(File.read(heroes_filename))
 responses = JSON.parse(File.read(responses_filename))
+n = responses.length
 
-def short_hero_name_from_url(url)
-  """Method that returns a short hero name for the given url
-    (taken from the filename on the Wiki server)."""
-  %r{/(\w+?)_.+?\.mp3}.match(url) do |search|
-    if search[1] == 'Dlc'
-      %r{/(Dlc_\w+?)_.+?\.mp3}.match(url) do |search_dlc|
-        if search_dlc[1] == 'Dlc_tech'
-          return 'Dlc_tech_ann'
-        else
-          return search_dlc[1]
-        end
-      end
-    end
-    return search[1]
-  end
-end
+responses.each_with_index do |r, i|
+  p "creating response #{i} / #{n}"
+  Response.create({
+    hero: r["hero"],
+    text: r["response"],
+    url: r["url"],
+    match: r["response"].downcase.gsub(/[^a-z]/, '')
+  })
 
-responses.each do |response, url|
-  short_hero = short_hero_name_from_url url
-  hero = heroes[short_hero]
-  p hero
-  Response.create(hero: hero, text: response, url: url, match: response.downcase.gsub(/[^a-z]/, ''))
 end
 
 p "Database created with #{Response.count} responses"
