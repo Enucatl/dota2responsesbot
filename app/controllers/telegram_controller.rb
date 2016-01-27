d6369f0a3d0fa10a56ce94f92f6b9c490d61e5fe
Matteo Abis <gmatteo..abis@gmail.com>
2016-01-27 14:41:13 +0100

    skip if message text is empty

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -2,16 +2,18 @@ class TelegramController < ApplicationController
 
   def show
     message = params[:message]
-    chat_id = message[:chat][:id]
-    found = ResponseFinder.new.find(message[:text]).to_a
-    unless found.empty?
-      hero_response = found.sample
-      bot_message = {
-        chat_id: chat_id,
-        reply_to_message_id: message[:message_id],
-        text: "#{hero_response[:hero]}: #{hero_response[:text]} #{hero_response[:url]}"
-      }
-      Telegram.sendMessage(bot_message)
+    if message.key?(:text)
+      chat_id = message[:chat][:id]
+      found = ResponseFinder.new.find(message[:text]).to_a
+      unless found.empty?
+        hero_response = found.sample
+        bot_message = {
+          chat_id: chat_id,
+          reply_to_message_id: message[:message_id],
+          text: "#{hero_response[:hero]}: #{hero_response[:text]} #{hero_response[:url]}"
+        }
+        Telegram.sendMessage(bot_message)
+      end
     end
     head :ok, content_type: "text/html"
   end
