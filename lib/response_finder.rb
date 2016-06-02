c7f80bf0bc5234610555fc8789f216679de699f2
Matteo Abis <gmatteo..abis@gmail.com>
2016-06-02 15:49:09 +0200

    introduce blacklist

--- lib/response_finder.rb
+++ lib/response_finder.rb
@@ -2,7 +2,20 @@ class ResponseFinder
 
   def find(text)
     to_be_matched = text.downcase.gsub(/[^a-z]/, '')
-    return Response.where(match: to_be_matched)
+    if blacklisted? to_be_matched
+      logger.debug "response blacklisted"
+      return []
+    else
+      return Response.where(match: to_be_matched)
+    end
   end
 
+  def blacklisted?(text)
+    blacklist = [
+      /^(ha)+$/
+    ]
+    re = Regexp.union(blacklist)
+    return re.match(text)
+  end
+  
 end
--- lib/telegram.rb
+++
@@ -1,9 +0,0 @@
-require "telegram/bot"
-
-class Telegramapi < Telegram::Bot::Api
-
-  def initialize
-    super(Rails.application.secrets[:telegram_token])
-  end
-
-end
